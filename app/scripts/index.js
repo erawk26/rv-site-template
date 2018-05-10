1d38e4ddd2c8c9062267848c416fe15fc8e08656
erik <erik@hark.bz>
2018-05-10 07:51:38 -0400

    extremly minor formatting

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,7 +1,5 @@
 import './../styles/main.scss';
-if (process.env.NODE_ENV !== 'production') {
-	require('./../index.pug');
-}
+if (process.env.NODE_ENV !== 'production') require('./../index.pug');
 require('./vueStuff');
 require('./scroll');
 require('./mobileMenu');
